b2be400f8ec92f79ad75eb0f832dd6a32c113c81
Ha Hai Nguyen <haiha.nguyen@etnetera.cz>
2018-06-06 17:20:45 +0200

    index.js wraps the app.js

--- src/CardList.js
+++ src/CardList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Card from './Card';
 
 const CardList = ({ robots }) => {
@@ -9,19 +8,17 @@ const CardList = ({ robots }) => {
             {
                 robots.map((user, i) => {
                     return (
-
                         <Card
                             key={i}
                             id={robots[i].id}
                             name={robots[i].name}
                             email={robots[i].email}
                         />
-
                     );
                 })
             }
         </div>
-    )
+    );
 }
 
 export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { robots } from './robots.js';
+
 
 ReactDOM.render(
-    <CardList robots={robots}/>,
+    <App/>,
     document.getElementById('root'));
 
 registerServiceWorker();
