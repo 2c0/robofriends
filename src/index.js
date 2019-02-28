f555873f1652fea7fde835ee48d9be3a08ba8a3c
Ha Hai Nguyen <haicorp87@gmail.com>
2019-02-28 15:00:37 +0100

    adding comments

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import {createStore} from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import {searchRobots} from "./reducers";
+import {searchRobots} from './reducers';
 import 'tachyons';
 
+// creating a store with root reducer
 const store = createStore(searchRobots);
 
 ReactDOM.render(
+    // store is now passed down to a props
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>,
     document.getElementById('root'));
 
