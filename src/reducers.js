c30494f7283eba70337697fbd44e36d37d0a0e3e
Ha Hai Nguyen <haicorp87@gmail.com>
2019-02-28 14:51:29 +0100

    updating reducer file, adding console log to check witch action was triggered based on the case

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,17 @@
 import {CHANGE_SEARCH_FIELD} from "./constants";
-import {SearchField} from "./actions";
 
+// set initial state here instead of the container component
 const initialState = {
   searchField: ''
 };
 
 export const searchRobots = (state = initialState, action = {}) => {
+  console.log(action.type);
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload});
+      //return an object with the initial state, plus new searchField property
+      // this is coming from actions.js file, type and payload
     default:
       return state;
   }
